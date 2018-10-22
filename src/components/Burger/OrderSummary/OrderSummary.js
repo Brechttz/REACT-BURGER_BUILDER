2287da3554c9d8cd3ead94c6d9af590414d76d4a
Brecht <brecht@moveup.care>
2018-10-22 10:26:10 +0200

    Add toolbar / logo + navigation

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients).map(igKey => {
@@ -16,7 +17,10 @@ const orderSummary = (props) => {
           <ul>
               {ingredientSummary}
           </ul>
+          <p><strong>Total Price: {props.price}</strong></p>
           <p>Continue to Checkout?</p>
+          <Button clicked={props.purchaseCancelled} btnType="Danger">CANCEL</Button>
+          <Button clicked={props.purchaseContinued} btnType="Success">CONTINUE</Button>
       </Aux>
     );
 };
